7f6525e3d7d4ed2d47743924316308b0f81ed0e3
Mabel Smith <msmith2401@icloud.com>
2020-07-14 14:52:59 -0400

    Initial Commit

---
+++ FinalProjectKWK2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProjectKWK2020
+//
+//  Created by Mabel Smith on 7/14/20.
+//  Copyright © 2020 Mabel Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
