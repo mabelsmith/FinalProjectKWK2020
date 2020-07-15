44411923880f118257b95fd8a1c0c6e9140b452e
Mabel Smith <msmith2401@icloud.com>
2020-07-15 18:03:38 -0400

    Wed July 15 Commit after camp

--- FinalProjectKWK2020/ViewController.swift
+++ FinalProjectKWK2020/ViewController.swift
@@ -6,16 +6,27 @@
 //  Copyright © 2020 Mabel Smith. All rights reserved.
 //
 
+
+    
+    
+    
 import UIKit
+import MapKit
+
+
 
 class ViewController: UIViewController {
+    
+     @IBOutlet weak var labelGitHubLessonTest: UILabel!
+   @IBOutlet private var mapView: MKMapView!
 
-    @IBOutlet weak var labelGitHubLessonTest: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+  }
 }
+
+//buildSetting = {
+//SWIFT_VERSION = 5;
+//OTHER_LDFLAGS = "-ObjC";
 
