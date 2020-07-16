b71f627bd080f523fa40afb3d838c44097893eca
Mabel Smith <msmith2401@icloud.com>
2020-07-16 16:18:09 -0400

    Day 9 Commit

---
+++ FinalProjectKWK2020/DirectoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DirectoryViewController.swift
+//  FinalProjectKWK2020
+//
+//  Created by Mabel Smith on 7/16/20.
+//  Copyright © 2020 Mabel Smith. All rights reserved.
+//
+
+import UIKit
+
+class DirectoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
