7eb08e4b378b2473ae33dffcdadfe31a471d7db1
Mabel Smith <msmith2401@icloud.com>
2020-07-16 11:58:03 -0400

    Day 9 commit test

--- FinalProjectKWK2020/ViewController.swift
+++ FinalProjectKWK2020/ViewController.swift
@@ -15,10 +15,75 @@ import MapKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     
      @IBOutlet weak var labelGitHubLessonTest: UILabel!
-   @IBOutlet private var mapView: MKMapView!
+
+    @IBOutlet private var mapView: MKMapView!
+    
+    //show route on map
+    func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
+
+    let sourcePlacemark = MKPlacemark(coordinate: pickupCoordinate, addressDictionary: nil)
+    let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate, addressDictionary: nil)
+
+    let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+    let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+
+    let sourceAnnotation = MKPointAnnotation()
+
+    if let location = sourcePlacemark.location {
+        sourceAnnotation.coordinate = location.coordinate
+    }
+
+    let destinationAnnotation = MKPointAnnotation()
+
+    if let location = destinationPlacemark.location {
+        destinationAnnotation.coordinate = location.coordinate
+    }
+
+    self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+
+        let directionRequest = MKDirections.Request()
+    directionRequest.source = sourceMapItem
+    directionRequest.destination = destinationMapItem
+    directionRequest.transportType = .automobile
+        
+ //calculate directions
+            let directions = MKDirections(request: directionRequest)
+
+            directions.calculate {
+                (response, error) -> Void in
+
+                guard let response = response else {
+                    if let error = error {
+                        print("Error: \(error)")
+                    }
+
+                    return
+                }
+
+                let route = response.routes[0]
+
+                self.mapView.addOverlay((route.polyline), level: MKOverlayLevel.aboveRoads)
+
+                let rect = route.polyline.boundingMapRect
+                self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+            }
+        }
+    
+    // - MKMapViewDelegate
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+
+        let renderer = MKPolylineRenderer(overlay: overlay)
+
+        renderer.strokeColor = UIColor(red: 17.0/255.0, green: 147.0/255.0, blue: 255.0/255.0, alpha: 1)
+
+        renderer.lineWidth = 5.0
+
+        return renderer
+    }
+       
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +91,6 @@ class ViewController: UIViewController {
   }
 }
 
-//buildSetting = {
-//SWIFT_VERSION = 5;
-//OTHER_LDFLAGS = "-ObjC";
+
+
 
